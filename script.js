0942026f8d7bc8cb51c9d6aaf2561f1e5cc91097
mmoralheredia <132943578+mmoralheredia@users.noreply.github.com>
2023-06-19 21:34:38 -0500

    Modificacion de funcionalidades, entradas de teclado, y modificaciones leves en apariencia de la calculadora

--- script.js
+++ script.js
@@ -1,134 +1,163 @@
-let currentScreen = document.getElementById('screenOne')
-let afterScreen = document.getElementById('screenTwo')
+let screen = document.getElementById('screen')
 let buttons = document.querySelectorAll('.keyboard input')
 
-currentScreen.value = '0'
-afterScreen.value = '0'
+screen.value = '0'
 
 let valueOne = ''
 let valueTwo = ''
 let currentOperation = ''
-let afterOperation = ''
-let secondValue = false
 let result = ''
+let secondValue = false
 let decimalPoint = false
+let lastButtonIsOperation = false
+let lastButtonIsEqual = false
 
+//Eventos de botones de teclado
+window.addEventListener("keydown", (event) => {
+    if (event.key >= "0" && event.key <= "9" || event.key === "." || event.key === "+"
+        || event.key === "-" || event.key === "*" || event.key === "/" || event.key === "Enter"
+        || event.key === "Backspace" || event.key === "Escape") {
+        buttons.forEach((button) => {
+            if (button.value === event.key) {
+                button.click()
+            } else if (event.key === "*" && button.value === "x") {
+                button.click()
+            } else if (event.key === "Enter" && button.value === "=") {
+                button.click()
+            } else if (event.key === "Backspace" && button.value === "<<") {
+                button.click()
+            } else if (event.key === "Escape" && button.value === "AC") {
+                button.click()
+            }
+        })
+    }
+})
+
+
+//Eventos de botones en pantalla
 buttons.forEach(button => {
     button.addEventListener('click', () => {
 
+        if (screen.value === "Error!") {
+            clearOrDelete("AC")
+        }
+
         if (button.id === "number") {
             writingNumbers(button.value)
-
         } else if (button.id === "operation") {
             if (button.value === '=') {
                 equalOperation()
             } else {
                 mathOperation(button.value)
-                afterOperation = currentOperation
             }
-
         } else if (button.id === "function") {
             clearOrDelete(button.value)
-
         } else if (button.id === 'decimalPoint') {
-            if (currentScreen.value.includes('.') === false) {
+            if (lastButtonIsEqual || lastButtonIsOperation) {
+                screen.value = '0.'
+                lastButtonIsEqual = false
+                lastButtonIsOperation = false
+            } else if (screen.value.includes('.') === false) {
                 decimalPoint = true
             } else {
                 null
             }
         }
     })
-});
+})
 
 
 //Declaración de las funciones utilizadas
 function writingNumbers(value) {
     if (decimalPoint === true) {
-        currentScreen.value += '.' + value
+        screen.value += '.' + value
         decimalPoint = false
-
-    } else if (currentScreen.value === '0') {
-        currentScreen.value = value
-
+    } else if (lastButtonIsOperation || lastButtonIsEqual) {
+        screen.value = value
+        lastButtonIsOperation = false
+        lastButtonIsEqual = false
+    } else if (screen.value === '0') {
+        screen.value = value
     } else {
-        currentScreen.value += value
+        screen.value += value
     }
 }
 
 function clearOrDelete(value) {
     if (value === 'AC') {
-        currentScreen.value = '0'
-        afterScreen.value = '0'
+        screen.value = '0'
         valueOne = ''
         valueTwo = ''
         currentOperation = ''
-        secondValue = false
         result = ''
+        secondValue = false
         decimalPoint = false
-
+        lastButtonIsEqual = false
+        lastButtonIsOperation = false
     } else if (value === '<<') {
-        if (currentScreen.value[currentScreen.value.length - 2] === '.') {
-            currentScreen.value = currentScreen.value.slice(0, -2)
-
-        } else if (decimalPoint === true) {
-            currentScreen.value = currentScreen.value.slice(0, -1)
-            decimalPoint = false
-
-        } else if (currentScreen.value.length === 1) {
-            currentScreen.value = '0'
-
+        if (screen.value[screen.value.length - 2] === '.') {
+            screen.value = screen.value.slice(0, -2)
+            lastButtonIsOperation = false
+        } else if (screen.value.length === 1) {
+            screen.value = "0"
         } else {
-            currentScreen.value = currentScreen.value.slice(0, -1)
+            screen.value = screen.value.slice(0, -1)
+            lastButtonIsOperation = false
         }
     }
 }
 
 function mathOperation(value) {
-    if (secondValue === true & currentScreen.value === '0') {
-        afterScreen.value = afterScreen.value.slice(0, -1)
+    if (lastButtonIsOperation) {
         currentOperation = value
-        afterScreen.value = afterScreen.value + value
-
-    } else if (secondValue === true & currentScreen.value != '0') {
-        console.log('estoy aquí')
+    } else if (secondValue & screen.value != '0') {
         equalOperation()
-
-        //!operación de encadenar operaciones
-
-    } else {
-        valueOne = parseFloat(currentScreen.value)
+        valueOne = parseFloat(screen.value)
         currentOperation = value
-        afterScreen.value = currentScreen.value + value
-        currentScreen.value = '0'
+        secondValue = true
+    } else {
+        valueOne = parseFloat(screen.value)
+        currentOperation = value
         secondValue = true
     }
+    lastButtonIsOperation = true
 }
 
 function equalOperation() {
-    afterScreen.value += currentScreen.value
-    valueTwo = parseFloat(currentScreen.value)
-
-    switch (currentOperation) {
-        case '+':
-            result = valueOne + valueTwo
-            break;
-        case '-':
-            result = valueOne - valueTwo
-            break;
-        case 'x':
-            result = valueOne * valueTwo
-            break;
-        case '/':
-            result = valueOne / valueTwo
-            break;
-        default:
-            break;
-    }
-
-    if (result == 'Infinity') {
-        result = 'Error'
+    if (!lastButtonIsEqual) {
+        valueTwo = parseFloat(screen.value)
     } else {
         null
     }
-    currentScreen.value = result
+
+    if (currentOperation != '') {
+        switch (currentOperation) {
+            case '+':
+                result = valueOne + valueTwo
+                break;
+            case '-':
+                result = valueOne - valueTwo
+                break;
+            case 'x':
+                result = valueOne * valueTwo
+                break;
+            case '/':
+                result = valueOne / valueTwo
+                break;
+            default:
+                break;
+        }
+        if (result == 'Infinity') {
+            result = 'Error!'
+        } else {
+            null
+        }
+        screen.value = result
+        valueOne = result
+        lastButtonIsEqual = true
+        lastButtonIsOperation = false
+        secondValue = false
+    } else {
+        null
+    }
 }
